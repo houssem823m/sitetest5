d37db84a62e35b16b7b4b7c48f9f17d5b0055c75
houssem823m <94017354+houssem823m@users.noreply.github.com>
2022-07-11 08:57:40 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+function signup() {
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
+    var numtel = document.getElementById('numtel').value;
+    alert('vous etes connecter avec success')
+    alert('votre Email est '+email)
+    alert('votre mot de passe est '+password)
+    alert('votre num de tel '+numtel)
+
+    
+}
